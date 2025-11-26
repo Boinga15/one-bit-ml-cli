5775387f467aa5de7a909c3f5f8f2b5e42d4a603
Boinga15 <realboinga@gmail.com>
2025-11-26 15:58:01 +0000

    Implemented Multi-Headed Attention block.

--- python/src/llm/parts.py
+++ python/src/llm/parts.py
@@ -38,6 +38,71 @@ class AddAndNormalize(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.layerNorm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-    
+
     def call(self, inputs):
         return self.layerNorm(tf.add_n(inputs))
+
+
+class ScaledDotProductAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def call(self, inputs):
+        q, k, v, mask = inputs
+        d_k = tf.cast(tf.shape(k)[-1], tf.float32)
+
+        scores = tf.matmul(q, k, transpose_b = True) / tf.math.sqrt(d_k)
+
+        if mask != None:
+            scores = scores + (mask * -1e9)
+        
+        weights = tf.nn.softmax(scores, axis=-1)
+
+        return tf.matmul(weights, v), weights
+
+
+class MultiHeadedAttention(tf.keras.layers.Layer):
+    def __init__(self, h, dModel, valueDimension, **kwargs):
+        super().__init__(**kwargs)
+        self.h = h
+        self.dModel = dModel
+        self.keyDimension = dModel // h
+        self.valueDimension = dModel // h
+
+        # Generating the linear layers.
+        self.linearQ = tf.keras.layers.Dense(dModel)
+        self.linearK = tf.keras.layers.Dense(dModel)
+        self.linearV = tf.keras.layers.Dense(dModel)
+
+        self.finalLinearLayer = tf.keras.layers.Dense(dModel)
+
+        # Also pre-preparing our SDPA instance.
+        self.SDPAInstance = ScaledDotProductAttention()
+    
+    def split_heads(self, x, dimension):
+        batch = tf.shape(x)[0]
+        seqLength = tf.shape(x)[1]
+
+        x = tf.reshape(x, (batch, seqLength, self.h, dimension))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        q, k, v, mask = inputs
+
+        # Create outputs for each linear set of linear layers.
+        outputs = [
+            self.split_heads(self.linearQ(q), self.keyDimension),
+            self.split_heads(self.linearK(k), self.keyDimension),
+            self.split_heads(self.linearV(v), self.valueDimension)
+        ]
+
+        # For each set of inputs, pass it through the scaled dot product attention.
+        context, attentionWeights = self.SDPAInstance.call([outputs[0], outputs[1], outputs[2], mask])
+
+        # Concatenate the results.
+        context = tf.transpose(context, [0, 2, 1, 3])
+        batch, seqLength = tf.shape(context)[0], tf.shape(context)[1]
+        context = tf.reshape(context, (batch, seqLength, self.valueDimension))
+
+        # Return the result of the final linear layer.
+        return self.finalLinearLayer(context)
